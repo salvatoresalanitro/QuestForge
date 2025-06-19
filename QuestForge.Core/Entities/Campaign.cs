9245a9113cbfeed8ad48836dce1c365c613c4ae6
Salvo <salvatoresalanitro94@gmail.com>
2025-06-19 17:16:23 +0200

    complete campaign entity

--- QuestForge.Core/Entities/Campaign.cs
+++ QuestForge.Core/Entities/Campaign.cs
@@ -8,5 +8,9 @@ namespace QuestForge.Core.Entities
 {
     internal class Campaign
     {
+        public Guid Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string? Description { get; set; }
+        public List<Character> Characters { get; set; } = [];
     }
 }
