8f28bfceec078b81d74bb65ca081f68f82ceee0d
Salvo <salvatoresalanitro94@gmail.com>
2025-06-20 11:01:30 +0200

    add context on program and connection string

--- QuestForge.API/Program.cs
+++ QuestForge.API/Program.cs
@@ -1,3 +1,6 @@
+using Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,8 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<QuestForgeContext>(options =>
+    options.UseSqlite(builder.Configuration.GetConnectionString("QuestForgeConnection")));
 
 var app = builder.Build();
 
