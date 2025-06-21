04549796b5be669c635366213dff9240bc50a29a
Salvo <salvatoresalanitro94@gmail.com>
2025-06-21 10:03:37 +0200

    create ICharacterService interface

---
+++ QuestForge.Core/Interfaces/Services/ICharacterService.cs
@@ -0,0 +1,10 @@
+﻿using QuestForge.Core.DTOs.Character;
+
+namespace QuestForge.Core.Interfaces.Services
+{
+    public interface ICharacterService
+    {
+        Task<CharacterDto?> GetByIdAsync(Guid id);
+        Task<CharacterDto> AddAsync(CreateCharacterDto createCharacterDto);
+    }
+}
