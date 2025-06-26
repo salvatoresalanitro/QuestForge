4b4932190f09bc1f7210d1b624ad2062cb06d409
Salvo <salvatoresalanitro94@gmail.com>
2025-06-26 18:51:14 +0200

    add validation for campaign and character

--- QuestForge.DTOs/DTOsCampaign/CreateCampaignDto.cs
+++ QuestForge.DTOs/DTOsCampaign/CreateCampaignDto.cs
@@ -1,8 +1,14 @@
-﻿namespace QuestForge.DTOs.DTOsCampaign
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace QuestForge.DTOs.DTOsCampaign
 {
     public class CreateCampaignDto
     {
+        [Required(ErrorMessage = "Name must be between 3 and 100 characters")]
+        [StringLength(100, MinimumLength = 5)]
         public string Name { get; set; } = string.Empty;
+
+        [StringLength(500)]
         public string? Description { get; set; }
     }
 }
--- QuestForge.DTOs/DTOsCharacter/CreateCharacterDto.cs
+++ QuestForge.DTOs/DTOsCharacter/CreateCharacterDto.cs
@@ -1,15 +1,31 @@
-﻿using QuestForge.Core.Entities;
+﻿using System.ComponentModel.DataAnnotations;
+using QuestForge.Core.Entities;
 
 namespace QuestForge.DTOs.DTOsCharacter
 {
     public class CreateCharacterDto
     {
+        [Required]
+        [StringLength(25, MinimumLength = 3)]
         public string Name { get; set; } = string.Empty;
+
+        [Required]
+        [EnumDataType(typeof(Species))]
         public Species Species { get; set; }
+
+        [EnumDataType(typeof(Class))]
         public Class Class { get; set; }
+
+        [Required(ErrorMessage = "Minimum level 1 and may not exceed 20")]
+        [Range(1, 20)]
         public int Level { get; set; }
+
+        [Required]
         public int HitPoints { get; set; }
+
+        [Required]
         public int ArmorClass { get; set; }
+
         public List<Item>? Items { get; set; }
     }
 }
