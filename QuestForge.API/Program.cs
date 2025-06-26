bc667e1fee311af8419f2f92cd5aa8e06f890bb1
Salvo <salvatoresalanitro94@gmail.com>
2025-06-26 18:35:56 +0200

    move db initialization from api to infrastructure

--- QuestForge.API/Program.cs
+++ QuestForge.API/Program.cs
@@ -1,9 +1,6 @@
-using Microsoft.EntityFrameworkCore;
 using QuestForge.Application.Interfaces;
 using QuestForge.Application.Services;
-using QuestForge.Core.Interfaces.RepositoryInterfaces;
-using QuestForge.Infrastructure.Data;
-using QuestForge.Infrastructure.Repositories;
+using QuestForge.Infrastructure;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -13,12 +10,9 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddDbContext<QuestForgeContext>(options =>
-    options.UseSqlite(builder.Configuration.GetConnectionString("QuestForgeConnection")));
+builder.Services.AddInfrastructure(builder.Configuration);
 
-builder.Services.AddScoped<ICampaignRepository, CampaignRepository>();
 builder.Services.AddScoped<ICampaignService, CampaignService>();
-builder.Services.AddScoped<ICharacterRepository, CharacterRepository>();
 builder.Services.AddScoped<ICharacterService, CharacterService>();
 
 var app = builder.Build();
---
+++ QuestForge.Infrastructure/ServiceCollectionExtensions.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using QuestForge.Core.Interfaces.RepositoryInterfaces;
+using QuestForge.Infrastructure.Data;
+using QuestForge.Infrastructure.Repositories;
+
+namespace QuestForge.Infrastructure
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection service, IConfiguration configuration)
+        {
+            service.AddDbContext<QuestForgeContext>(options =>
+                options.UseSqlite(configuration.GetConnectionString("QuestForgeConnection")));
+
+            service.AddScoped<ICampaignRepository, CampaignRepository>();
+            service.AddScoped<ICharacterRepository, CharacterRepository>();
+
+            return service;
+        }
+    }
+}
