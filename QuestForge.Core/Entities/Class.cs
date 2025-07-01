57bdb5da9c567e66a3bee607bc4c63896c13e8a9
Salvo <salvatoresalanitro94@gmail.com>
2025-07-01 18:08:02 +0200

    add reference to class and species

--- QuestForge.Core/Entities/Class.cs
+++ QuestForge.Core/Entities/Class.cs
@@ -4,6 +4,7 @@
     {
         public int Id { get; init; }
         public string Name { get; private set; } = string.Empty;
+        public List<SubClass> SubClasses { get; private set; } = [];
 
         public Class(int id, string name)
         {
--- QuestForge.Core/Entities/Species.cs
+++ QuestForge.Core/Entities/Species.cs
@@ -4,6 +4,7 @@
     {
         public int Id { get; init; }
         public string Name { get; private set; } = string.Empty;
+        public List<SubSpecies> SubSpecies { get; private set; } = [];
 
         public Species(int id, string name)
         {
