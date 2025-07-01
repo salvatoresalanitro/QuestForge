8daf8f1c056469c8cf13edb87eb85607d34d4225
Salvo <salvatoresalanitro94@gmail.com>
2025-07-01 18:07:29 +0200

    add subclass and subspecies entities

---
+++ QuestForge.Core/Entities/SubClass.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuestForge.Core.Entities
+{
+    public class SubClass
+    {
+        public int Id { get; init; }
+        public string Name { get; private set; } = string.Empty;
+        public int ClassId { get; private set; }
+        public Class Class { get; private set; } = null!;
+
+        public SubClass(int id, string name, int classId)
+        {
+            Id = id;
+            Name = name;
+            ClassId = classId;
+        }
+    }
+}
---
+++ QuestForge.Core/Entities/SubSpecies.cs
@@ -0,0 +1,16 @@
+﻿namespace QuestForge.Core.Entities
+{
+    public class SubSpecies
+    {
+        public int Id { get; init; }
+        public string Name { get; private set; } = string.Empty;
+        public int SpeciesId { get; private set; }
+        public Species Species { get; private set; } = null!;
+        public SubSpecies(int id, string name, int speciesId)
+        {
+            Id = id;
+            Name = name;
+            SpeciesId = speciesId;
+        }
+    }
+}
