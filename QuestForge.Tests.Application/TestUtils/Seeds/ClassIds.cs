c31c6082d6c5cdaad8f0e81011ac9dd104ff07c1
Salvo <salvatoresalanitro94@gmail.com>
2025-07-02 11:16:59 +0200

    add test utils seeds data for classId and speciesId

---
+++ QuestForge.Tests.Application/TestUtils/Seeds/ClassIds.cs
@@ -0,0 +1,18 @@
+﻿namespace QuestForge.Tests.Application.TestUtils.Seeds
+{
+    public static class ClassIds
+    {
+        public const int Barbarian = 1;
+        public const int Bard = 2;
+        public const int Cleric = 3;
+        public const int Druid = 4;
+        public const int Fighter = 5;
+        public const int Monk = 6;
+        public const int Paladin = 7;
+        public const int Ranger = 8;
+        public const int Rogue = 9;
+        public const int Sorcerer = 10;
+        public const int Warlock = 11;
+        public const int Wizard = 12;
+    }
+}
---
+++ QuestForge.Tests.Application/TestUtils/Seeds/SpeciesIds.cs
@@ -0,0 +1,16 @@
+﻿namespace QuestForge.Tests.Application.TestUtils.Seeds
+{
+    public static class SpeciesIds
+    {
+        public const int Aasimar = 1;
+        public const int Dragonborn = 2;
+        public const int Dwarf = 3;
+        public const int Elf = 4;
+        public const int Gnome = 5;
+        public const int Goliath = 6;
+        public const int Halfling = 7;
+        public const int Human = 8;
+        public const int Orc = 9;
+        public const int Tiefling = 10;
+    }
+}
