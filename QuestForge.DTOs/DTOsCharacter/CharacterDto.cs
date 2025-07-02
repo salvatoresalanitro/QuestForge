6991fc96ab32be54c6715b2ab20c3aca28eade98
Salvo <salvatoresalanitro94@gmail.com>
2025-07-02 10:28:14 +0200

    fix warnings in characterDto

--- QuestForge.DTOs/DTOsCharacter/CharacterDto.cs
+++ QuestForge.DTOs/DTOsCharacter/CharacterDto.cs
@@ -6,8 +6,8 @@ namespace QuestForge.DTOs.DTOsCharacter
     {
         public Guid Id { get; set; }
         public string Name { get; set; } = string.Empty;
-        public Species Species { get; set; }
-        public Class Class { get; set; }
+        public Species Species { get; set; } = null!;
+        public Class Class { get; set; } = null!;
         public int Level { get; set; }
         public int HitPoints { get; set; }
         public int ArmorClass { get; set; }
