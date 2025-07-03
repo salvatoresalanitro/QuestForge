f7b64e53e70696fd8586209946b92d7c83581c50
Salvo <salvatoresalanitro94@gmail.com>
2025-07-03 11:16:19 +0200

    add item type ids utils test seed

---
+++ QuestForge.Tests.Application/TestUtils/Seeds/ItemTypeIds.cs
@@ -0,0 +1,13 @@
+﻿namespace QuestForge.Tests.Application.TestUtils.Seeds
+{
+    public class ItemTypeIds
+    {
+        public const int Weapon = 1;
+        public const int MagicWeapon = 2;
+        public const int Armor = 3;
+        public const int MagicArmor = 4;
+        public const int Equipment = 5;
+        public const int MagicEquipment = 6;
+        public const int Tool = 7;
+    }
+}
