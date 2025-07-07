78d7fbe356bc41146debbf04861382536da84b51
Salvo <salvatoresalanitro94@gmail.com>
2025-07-07 10:54:10 +0200

    add character model

---
+++ QuestForge.Domain/Characters/CharacterModel.cs
@@ -0,0 +1,20 @@
+﻿using QuestForge.Domain.Campaigns;
+using QuestForge.Domain.Items;
+using QuestForge.Domain.ValueObjects;
+
+namespace QuestForge.Domain.Characters
+{
+    public class CharacterModel
+    {
+        public Guid Id { get; init; }
+        public string Name { get; private set; } = string.Empty;
+        public Species Species { get; private set; }
+        public Class Class { get; private set; }
+        public int Level { get; init; }
+        public int HitPoints { get; init; }
+        public int ArmorClass { get; init; }
+        public List<ItemModel> Items { get; init; } = [];
+        public CampaignModel Campaign { get; private set; }
+        public Guid CampaignId { get; private set; }
+    }
+}
