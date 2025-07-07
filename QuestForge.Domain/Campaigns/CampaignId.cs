c26cc7e9ed0411a9aae1a116ddb3a308536452d0
Salvo <salvatoresalanitro94@gmail.com>
2025-07-07 13:09:05 +0200

    complete campaign id class

--- QuestForge.Domain/Campaigns/CampaignId.cs
+++ QuestForge.Domain/Campaigns/CampaignId.cs
@@ -2,6 +2,16 @@
 {
     public record CampaignId
     {
+        public Guid Id { get; init; }
 
+        private CampaignId()
+        {
+            Id = Guid.NewGuid();
+        }
+
+        public static CampaignId Create()
+        {
+            return new CampaignId();
+        }
     }
 }
