5f3e9526554b15df918ec8ed47fad448f5bd1497
Salvo <salvatoresalanitro94@gmail.com>
2025-07-07 11:59:33 +0200

    move models in model folder inside infrastructure project

--- QuestForge.Infrastructure/Models/CharacterModel.cs
+++ QuestForge.Infrastructure/Models/CharacterModel.cs
@@ -7,14 +7,13 @@ namespace QuestForge.Domain.Characters
     public class CharacterModel
     {
         public Guid Id { get; init; }
-        public string Name { get; private set; } = string.Empty;
-        public Species Species { get; private set; }
-        public Class Class { get; private set; }
+        public string Name { get; init; } = string.Empty;
+        public Species Species { get; init; }
+        public Class Class { get; init; }
         public int Level { get; init; }
         public int HitPoints { get; init; }
         public int ArmorClass { get; init; }
         public List<ItemModel> Items { get; init; } = [];
-        public CampaignModel Campaign { get; private set; }
-        public Guid CampaignId { get; private set; }
+        public CampaignModel Campaign { get; init; }
     }
 }
