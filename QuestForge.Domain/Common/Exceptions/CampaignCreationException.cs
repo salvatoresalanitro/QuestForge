854642c21c5b7b7a79c7cb98326690a79573cdd0
Salvo <salvatoresalanitro94@gmail.com>
2025-07-08 09:56:51 +0200

    add custom exception for campaign creation exception

---
+++ QuestForge.Domain/Common/Exceptions/CampaignCreationException.cs
@@ -0,0 +1,11 @@
+﻿namespace QuestForge.Domain.Common.Exceptions
+{
+    public class CampaignCreationException : DomainException
+    {
+        public CampaignCreationException() { }
+
+        public CampaignCreationException(string message) : base(message) { }
+
+        public CampaignCreationException(string message, Exception innerException) : base(message, innerException) { }
+    }
+}
