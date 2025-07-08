037d87c53461b91e04527e14b00bf56f9fada063
Salvo <salvatoresalanitro94@gmail.com>
2025-07-08 10:00:49 +0200

    add VO campaignName

---
+++ QuestForge.Domain/ValueObjects/CampaignVO/CampaignName.cs
@@ -0,0 +1,36 @@
+﻿using QuestForge.Domain.Common.Exceptions;
+
+namespace QuestForge.Domain.ValueObjects.CampaignVO
+{
+    public sealed record CampaignName
+    {
+        public string Value { get; }
+
+        private CampaignName(string value)
+        {
+            if(string.IsNullOrWhiteSpace(value))
+            {
+                throw new CampaignCreationException("Name cannot be empty.");
+            }
+
+            if(value.Length < 5)
+            {
+                throw new CampaignCreationException("Name cannot be less than 5 characters.");
+            }
+
+            if(value.Length > 100)
+            {
+                throw new CampaignCreationException("Name cannot exceed 100 characters.");
+            }
+
+            Value = value.Trim();
+        }
+
+        public static CampaignName Create(string value)
+        {
+            return new CampaignName(value);
+        }
+
+        public override string ToString() => Value;
+    }
+}
