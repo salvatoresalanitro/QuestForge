96d9f7c8ee335eb1bb073447b775205349bef292
Salvo <salvatoresalanitro94@gmail.com>
2025-07-08 10:13:04 +0200

    add VO campaign description

---
+++ QuestForge.Domain/ValueObjects/CampaignVO/CampaignDescription.cs
@@ -0,0 +1,24 @@
+﻿using QuestForge.Domain.Common.Exceptions;
+
+namespace QuestForge.Domain.ValueObjects.CampaignVO
+{
+    public sealed record CampaignDescription
+    {
+        public string Value { get; } = string.Empty;
+
+        private CampaignDescription(string value)
+        {
+            if(value.Length > 500)
+            {
+                throw new CampaignCreationException("Description cannot exceed 500 characters.");
+            }
+
+            Value = value;
+        }
+
+        public static CampaignDescription Create(string value)
+        {
+            return new CampaignDescription(value);
+        }
+    }
+}
