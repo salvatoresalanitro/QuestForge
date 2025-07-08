3fad564c544aeb487e619d6026ba4de2842fcb4a
Salvo <salvatoresalanitro94@gmail.com>
2025-07-08 09:48:16 +0200

    first draft campaign domain aggregate

---
+++ QuestForge.Domain/Campaigns/Campaign.cs
@@ -0,0 +1,18 @@
+﻿using QuestForge.Domain.Characters;
+using QuestForge.Domain.Items;
+
+namespace QuestForge.Domain.Campaigns
+{
+    public class Campaign
+    {
+        public Guid Id { get; }
+        public string Name { get; }
+        public string Description { get; }
+        private List<Character> _characters = [];
+        public IReadOnlyCollection<Character> Characters => _characters;
+        private readonly List<Item> _items = [];
+        public IReadOnlyCollection<Item> Items => _items;
+        //public List<Npc> Npcs { get; init; }
+        //public List<Enemy> Enemies { get; init; }
+    }
+}
