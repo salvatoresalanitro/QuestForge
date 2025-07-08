3a4d607998af72f0fc184d605974132cc87fa2a4
Salvo <salvatoresalanitro94@gmail.com>
2025-07-08 16:52:02 +0200

    fix item model

--- QuestForge.Infrastructure/Models/ItemModel.cs
+++ QuestForge.Infrastructure/Models/ItemModel.cs
@@ -1,14 +1,12 @@
-﻿using QuestForge.Core.Entities;
-
-namespace QuestForge.Infrastructure.Models
+﻿namespace QuestForge.Infrastructure.Models
 {
     public class ItemModel
     {
         public Guid Id { get; init; }
         public string Name { get; init; } = string.Empty;
         public string Description { get; init; } = string.Empty;
-        public ItemType Type { get; init; } = null!;
-        public Character? Character { get; init; }
-        public Campaign? Campaign { get; init; } = null!;
+        public ItemTypeModel Type { get; init; } = null!;
+        public CharacterModel? Character { get; init; }
+        public CampaignModel? Campaign { get; init; }
     }
 }
