e5de69e396521d325565798c27b5b33b56436c30
Salvo <salvatoresalanitro94@gmail.com>
2025-07-09 10:32:12 +0200

    add custom excpetion for item creation

---
+++ QuestForge.Domain/Common/Exceptions/ItemCreationException.cs
@@ -0,0 +1,11 @@
+﻿namespace QuestForge.Domain.Common.Exceptions
+{
+    public class ItemCreationException : DomainException
+    {
+        public ItemCreationException() { }
+
+        public ItemCreationException(string message) : base(message) { }
+
+        public ItemCreationException(string message, Exception innerException) : base(message, innerException) { }
+    }
+}
