e8669869d22562d6e12aa5340ede195b68ec2a3a
Salvo <salvatoresalanitro94@gmail.com>
2025-07-09 12:02:51 +0200

    create species and subspecies mapping

---
+++ QuestForge.Infrastructure/Mapping/SpeciesModelMapping.cs
@@ -0,0 +1,38 @@
+﻿using QuestForge.Domain.ValueObjects;
+using QuestForge.Infrastructure.Models;
+
+namespace QuestForge.Infrastructure.Mapping
+{
+    public static class SpeciesModelMapping
+    {
+        public static Species MapToDomain(this SpeciesModel model)
+        {
+            var subSpecies = model.AllSubSpecies
+                .Select(s => SubSpecies.Create(s.Id, s.Name, s.Species.MapToDomain()))
+                .ToList();
+
+            return Species.Create(
+                model.Id,
+                model.Name,
+                subSpecies    
+            );
+        }
+
+        public static SpeciesModel MapToModel(this Species species)
+        {
+            var model = new SpeciesModel()
+            {
+                Id = species.Id,
+                Name = species.Name
+            };
+
+            model.AllSubSpecies.AddRange(
+                species.AllSubSpecies
+                    .Select(s => s.MapToModel()
+                )
+            );
+
+            return model;
+        }
+    }
+}
---
+++ QuestForge.Infrastructure/Mapping/SubSpeciesModelMapping.cs
@@ -0,0 +1,27 @@
+﻿using QuestForge.Domain.ValueObjects;
+using QuestForge.Infrastructure.Models;
+
+namespace QuestForge.Infrastructure.Mapping
+{
+    public static class SubSpeciesModelMapping
+    {
+        public static SubSpecies MapToDomain(this SubSpeciesModel model)
+        {
+            return SubSpecies.Create(
+                model.Id,
+                model.Name,
+                model.Species.MapToDomain()
+            );
+        }
+
+        public static SubSpeciesModel MapToModel(this SubSpecies domain)
+        {
+            return new SubSpeciesModel()
+            {
+                Id = domain.Id,
+                Name = domain.Name,
+                Species = domain.Species.MapToModel()
+            };
+        }
+    }
+}
