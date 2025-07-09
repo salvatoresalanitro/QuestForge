6443d883b54c609f1e3c06afa84aba8fe55e2497
Salvo <salvatoresalanitro94@gmail.com>
2025-07-09 11:01:29 +0200

    order property character model

--- QuestForge.Infrastructure/Models/CharacterModel.cs
+++ QuestForge.Infrastructure/Models/CharacterModel.cs
@@ -4,11 +4,11 @@
     {
         public Guid Id { get; init; }
         public string Name { get; init; } = string.Empty;
-        public SpeciesModel Species { get; init; } = null!;
-        public ClassModel Class { get; init; } = null!;
         public int Level { get; init; }
         public int HitPoints { get; init; }
         public int ArmorClass { get; init; }
+        public SpeciesModel Species { get; init; } = null!;
+        public ClassModel Class { get; init; } = null!;
         public List<ItemModel> Items { get; init; } = [];
         public CampaignModel? Campaign { get; init; }
     }
