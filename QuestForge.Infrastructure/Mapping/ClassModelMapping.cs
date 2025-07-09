c2c3062c4171eacac7d6bb8b29d77a97de955fb5
Salvo <salvatoresalanitro94@gmail.com>
2025-07-09 12:31:13 +0200

    add mapping model for class and subclass

---
+++ QuestForge.Infrastructure/Mapping/ClassModelMapping.cs
@@ -0,0 +1,36 @@
+﻿using QuestForge.Domain.ValueObjects;
+using QuestForge.Infrastructure.Models;
+
+namespace QuestForge.Infrastructure.Mapping
+{
+    public static class ClassModelMapping
+    {
+        public static Class MapToDomain(this ClassModel model)
+        {
+            var subClasses = model.SubClasses
+                .Select(scM => SubClass.Create(scM.Id, scM.Name, scM.Class.MapToDomain()))
+                .ToList();
+
+            return Class.Create(
+                model.Id,
+                model.Name,
+                subClasses
+            );
+        }
+
+        public static ClassModel MapToModel(this Class domain)
+        {
+            var model = new ClassModel()
+            {
+                Id = domain.Id,
+                Name = domain.Name,
+            };
+
+            model.SubClasses.AddRange(
+                domain.SubClasses.Select(c => c.MapToModel())
+            );
+
+            return model;
+        }
+    }
+}
---
+++ QuestForge.Infrastructure/Mapping/SubClassModelMapping.cs
@@ -0,0 +1,27 @@
+﻿using QuestForge.Domain.ValueObjects;
+using QuestForge.Infrastructure.Models;
+
+namespace QuestForge.Infrastructure.Mapping
+{
+    public static class SubClassModelMapping
+    {
+        public static SubClass MapToDomain(this SubClassModel model)
+        {
+            return SubClass.Create(
+                model.Id,
+                model.Name,
+                model.Class.MapToDomain()
+            );
+        }
+
+        public static SubClassModel MapToModel(this SubClass domain)
+        {
+            return new SubClassModel()
+            {
+                Id = domain.Id,
+                Name = domain.Name,
+                Class = domain.Class.MapToModel(),
+            };
+        }
+    }
+}
