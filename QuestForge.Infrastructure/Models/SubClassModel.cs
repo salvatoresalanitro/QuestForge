70c7bec2b7193e469897c5d0cf7c8a051a7d96ee
Salvo <salvatoresalanitro94@gmail.com>
2025-07-09 12:30:10 +0200

    add FK to class in subclass

--- QuestForge.Infrastructure/Models/SubClassModel.cs
+++ QuestForge.Infrastructure/Models/SubClassModel.cs
@@ -4,5 +4,7 @@
     {
         public int Id { get; init; }
         public string Name { get; init; } = string.Empty;
+        public int ClassId { get; init; }
+        public ClassModel Class { get; init; } = null!;
     }
 }
