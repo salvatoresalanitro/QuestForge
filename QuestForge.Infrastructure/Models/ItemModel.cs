c2e774e6be886aa41c7b707ef0a0d01d939b2891
Salvo <salvatoresalanitro94@gmail.com>
2025-07-09 15:32:48 +0200

    add FK type id to item model

--- QuestForge.Infrastructure/Models/ItemModel.cs
+++ QuestForge.Infrastructure/Models/ItemModel.cs
@@ -5,6 +5,7 @@
         public Guid Id { get; init; }
         public string Name { get; init; } = string.Empty;
         public string Description { get; init; } = string.Empty;
+        public int TypeId { get; init; }
         public ItemTypeModel Type { get; init; } = null!;
         public CharacterModel? Character { get; init; }
         public CampaignModel? Campaign { get; init; }
