80af941d89eac0745565310034fd34a2c4c715c7
Salvo <salvatoresalanitro94@gmail.com>
2025-07-09 10:33:05 +0200

    complete item with factory methods

--- QuestForge.Domain/Items/Item.cs
+++ QuestForge.Domain/Items/Item.cs
@@ -1,7 +1,50 @@
-﻿namespace QuestForge.Domain.Items
+﻿using QuestForge.Domain.Campaigns;
+using QuestForge.Domain.Characters;
+using QuestForge.Domain.Items.ItemVO;
+using QuestForge.Domain.ValueObjects;
+
+namespace QuestForge.Domain.Items
 {
     public class Item
     {
+        public ItemId Id { get; }
+        public ItemName Name { get; }
+        public ItemDescription Description { get; }
+        public ItemType Type { get; }
+        public Character? Character { get; }
+        public Campaign? Campaign { get; }
 
+        private Item(
+            Guid id,
+            string name,
+            string description,
+            ItemType type
+        )
+        {
+            Id = ItemId.Create(id);
+            Name = ItemName.Create(name);
+            Description = ItemDescription.Create(description);
+            Type = type;
+        }
+
+        public static Item Create(
+            Guid id,
+            string name,
+            string description,
+            ItemType type
+        )
+        {
+            return new Item(id, name, description, type);
+        }
+
+        public static Item Reconstitute(
+            ItemId id,
+            ItemName name,
+            ItemDescription description,
+            ItemType type
+        )
+        {
+            return new Item(id.Value, name.Value, description.Value, type);
+        }
     }
 }
