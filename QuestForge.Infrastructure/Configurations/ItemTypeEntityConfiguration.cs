c00114e057fe06676d0d8404c5a7ba565e54f4a0
Salvo <salvatoresalanitro94@gmail.com>
2025-07-09 15:33:20 +0200

    add item type entity configuration

---
+++ QuestForge.Infrastructure/Configurations/ItemTypeEntityConfiguration.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using QuestForge.Infrastructure.Models;
+
+namespace QuestForge.Infrastructure.Configurations
+{
+    public class ItemTypeEntityConfiguration : IEntityTypeConfiguration<ItemTypeModel>
+    {
+        public void Configure(EntityTypeBuilder<ItemTypeModel> builder)
+        {
+            builder.ToTable("ItemType");
+
+            builder.HasKey(it => it.Id);
+
+            builder.Property(it => it.Id)
+               .HasColumnName("Id")
+               .ValueGeneratedNever();
+
+            builder.Property(it => it.Name)
+                .IsRequired();
+
+            builder.HasMany(it => it.Items)
+                .WithOne(i => i.Type)
+                .HasForeignKey(i => i.TypeId)
+                .OnDelete(DeleteBehavior.Restrict);
+        }
+    }
+}
