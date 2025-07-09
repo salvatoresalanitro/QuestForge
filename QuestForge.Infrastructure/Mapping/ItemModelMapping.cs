46151c263256e641cf3630de27d27d2ec0edf738
Salvo <salvatoresalanitro94@gmail.com>
2025-07-09 11:00:47 +0200

    add item modelmapping and item type model mapping

---
+++ QuestForge.Infrastructure/Mapping/ItemModelMapping.cs
@@ -0,0 +1,30 @@
+﻿using QuestForge.Domain.Items;
+using QuestForge.Domain.Items.ItemVO;
+using QuestForge.Infrastructure.Models;
+
+namespace QuestForge.Infrastructure.Mapping
+{
+    public static class ItemModelMapping
+    {
+        public static Item MapToDomain(this ItemModel model)
+        {
+            return Item.Reconstitute(
+                ItemId.Create(model.Id),
+                ItemName.Create(model.Name),
+                ItemDescription.Create(model.Description),
+                model.Type.MapToDomain()
+            );
+        }
+
+        public static ItemModel MapToModel(this Item item)
+        {
+            return new ItemModel()
+            {
+                Id = item.Id.Value,
+                Name = item.Name.Value,
+                Description = item.Description.Value,
+                Type = item.Type.MapToModel()
+            };
+        }
+    }
+}
---
+++ QuestForge.Infrastructure/Mapping/ItemTypeMapping.cs
@@ -0,0 +1,29 @@
+﻿using QuestForge.Domain.ValueObjects;
+using QuestForge.Infrastructure.Models;
+
+namespace QuestForge.Infrastructure.Mapping
+{
+    public static class ItemTypeMapping
+    {
+        public static ItemType MapToDomain(this ItemTypeModel model)
+        {
+            return ItemType.Create(
+                model.Id,
+                model.Name
+            );
+        }
+
+        public static ItemTypeModel MapToModel(this ItemType itemType)
+        {
+            var model = new ItemTypeModel()
+            {
+                Id = itemType.Id,
+                Name = itemType.Name
+            };
+
+            model.Items.AddRange(itemType.Items.Select(i => i.MapToModel()));
+
+            return model;
+        }
+    }
+}
