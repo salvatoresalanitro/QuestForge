6d74898c707080163740f7eaf5858560c1b740ff
Salvo <salvatoresalanitro94@gmail.com>
2025-07-11 12:33:29 +0200

    add get all query for campaign

---
+++ QuestForge.Application/UsesCases/Queries/Campaigns/GetAllCampaigns/GetAllCampaignsQueryHandler.cs
@@ -0,0 +1,27 @@
+﻿using MediatR;
+using QuestForge.Application.Exceptions;
+using QuestForge.Application.Mapping;
+using QuestForge.Domain.Campaigns;
+using QuestForge.DTOs.DTOsCampaign;
+
+namespace QuestForge.Application.UsesCases.Queries.Campaigns.GetAllCampaigns
+{
+    public sealed record GetAllCampaignsQueryHandler : IRequestHandler<GetAllCampaignsQuery, IEnumerable<CampaignDto>>
+    {
+        private readonly ICampaignRepository _campaignRepository;
+
+        public GetAllCampaignsQueryHandler(ICampaignRepository campaignRepository)
+        {
+            _campaignRepository = campaignRepository;
+        }
+
+        public async Task<IEnumerable<CampaignDto>> Handle(GetAllCampaignsQuery request, CancellationToken cancellationToken)
+        {
+            var campaigns = await _campaignRepository.GetAllAsync(cancellationToken);
+
+            return campaigns is null
+                ? throw new CampaignNotFoundException("No campaigns found.")
+                : campaigns.Select(c => CampaignMapper.ToDto(c));
+        }
+    }
+}
