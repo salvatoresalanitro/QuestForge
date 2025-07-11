8989deea250d6b6ffcff718e51dace10894ea040
Salvo <salvatoresalanitro94@gmail.com>
2025-07-11 12:19:50 +0200

    comment service to build, remove in future

--- QuestForge.Application/Services/CampaignService.cs
+++ QuestForge.Application/Services/CampaignService.cs
@@ -1,64 +1,63 @@
 ﻿using QuestForge.Application.Interfaces;
 using QuestForge.Application.Mapping;
-using QuestForge.Core.Interfaces.RepositoryInterfaces;
 using QuestForge.DTOs.DTOsCampaign;
 
 namespace QuestForge.Application.Services
 {
-    public class CampaignService : ICampaignService
+    public class CampaignService
     {
-        private readonly ICampaignRepository _repository;
+        //private readonly ICampaignRepository _repository;
 
-        public CampaignService(ICampaignRepository repository)
-        {
-            _repository = repository;
-        }
+        //public CampaignService(ICampaignRepository repository)
+        //{
+        //    _repository = repository;
+        //}
 
-        public async Task<CampaignDto> CreateAsync(CreateCampaignDto createDto)
-        {
-            var campaign = CampaignMapper.ToEntity(createDto);
-            await _repository.AddAsync(campaign);
+        //public async Task<CampaignDto> CreateAsync(CreateCampaignDto createDto)
+        //{
+        //    var campaign = CampaignMapper.ToEntity(createDto);
+        //    await _repository.AddAsync(campaign);
 
-            return CampaignMapper.ToDto(campaign);
-        }
+        //    return CampaignMapper.ToDto(campaign);
+        //}
 
-        public async Task<bool> DeleteAsync(Guid id)
-        {
-            var campaign = await _repository.GetByIdAsync(id);
-            if(campaign is null)
-            {
-                return false;
-            }
+        //public async Task<bool> DeleteAsync(Guid id)
+        //{
+        //    var campaign = await _repository.GetByIdAsync(id);
+        //    if(campaign is null)
+        //    {
+        //        return false;
+        //    }
 
-            await _repository.DeleteAsync(campaign);
-            return true;
-        }
+        //    await _repository.DeleteAsync(campaign);
+        //    return true;
+        //}
 
-        public async Task<IEnumerable<CampaignDto>> GetAllAsync()
-        {
-            var campaigns = await _repository.GetAllAsync();
+        //public async Task<IEnumerable<CampaignDto>> GetAllAsync()
+        //{
+        //    var campaigns = await _repository.GetAllAsync();
 
-            return campaigns.Select(c => CampaignMapper.ToDto(c));
-        }
+        //    return campaigns.Select(c => CampaignMapper.ToDto(c));
+        //}
 
-        public async Task<CampaignDto?> GetByIdAsync(Guid id)
-        {
-            var campaign = await _repository.GetByIdAsync(id);
-            return campaign is null ? null : CampaignMapper.ToDto(campaign);
-        }
+        //public async Task<CampaignDto?> GetByIdAsync(Guid id)
+        //{
+        //    var campaign = await _repository.GetByIdAsync(id);
+        //    return campaign is null ? null : CampaignMapper.ToDto(campaign);
+        //}
 
-        public async Task<CampaignDto?> UpdateAsync(Guid id, CreateCampaignDto createDto)
-        {
-            var campaign = await _repository.GetByIdAsync(id);
-            if (campaign is null)
-            {
-                return null;
-            }
+        //public async Task<CampaignDto?> UpdateAsync(Guid id, CreateCampaignDto createDto)
+        //{
+        //    var campaign = await _repository.GetByIdAsync(id);
+        //    if (campaign is null)
+        //    {
+        //        return null;
+        //    }
 
-            campaign.Update(createDto.Name, createDto.Description ?? string.Empty);
+        //    campaign.Update(createDto.Name, createDto.Description ?? string.Empty);
 
-            await _repository.UpdateAsync(campaign);
-            return CampaignMapper.ToDto(campaign);
-        }
+        //    await _repository.UpdateAsync(campaign);
+        //    return CampaignMapper.ToDto(campaign);
+        //}
     }
 }
--- QuestForge.Application/Services/CharacterService.cs
+++ QuestForge.Application/Services/CharacterService.cs
@@ -1,84 +1,78 @@
-﻿using QuestForge.Application.Interfaces;
-using QuestForge.Application.Mapping;
-using QuestForge.Core.Exceptions;
-using QuestForge.Core.Interfaces.RepositoryInterfaces;
-using QuestForge.DTOs.DTOsCharacter;
-
-namespace QuestForge.Application.Services
+﻿namespace QuestForge.Application.Services
 {
-    public class CharacterService : ICharacterService
+    public class CharacterService
     {
-        private readonly ICharacterRepository _characterRepository;
-        private readonly ISpeciesRepository _speciesRepository;
-        private readonly IClassRepository _classRepository;
+        //private readonly ICharacterRepository _characterRepository;
+        //private readonly ISpeciesRepository _speciesRepository;
+        //private readonly IClassRepository _classRepository;
 
-        public CharacterService(ICharacterRepository characterRepository, ISpeciesRepository speciesRepository, IClassRepository classRepository)
-        {
-            _characterRepository = characterRepository;
-            _speciesRepository = speciesRepository;
-            _classRepository = classRepository;
-        }
+        //public CharacterService(ICharacterRepository characterRepository, ISpeciesRepository speciesRepository, IClassRepository classRepository)
+        //{
+        //    _characterRepository = characterRepository;
+        //    _speciesRepository = speciesRepository;
+        //    _classRepository = classRepository;
+        //}
 
-        public async Task<CharacterDto> CreateAsync(CreateCharacterDto createCharacterDto)
-        {
-            var species = await _speciesRepository.GetByIdAsync(createCharacterDto.SpeciesId);
-            var @class = await _classRepository.GetByIdAsync(createCharacterDto.ClassId);
+        //public async Task<CharacterDto> CreateAsync(CreateCharacterDto createCharacterDto)
+        //{
+        //    var species = await _speciesRepository.GetByIdAsync(createCharacterDto.SpeciesId);
+        //    var @class = await _classRepository.GetByIdAsync(createCharacterDto.ClassId);
 
-            if(species == null || @class == null)
-            {
-                throw new InvalidOperationException("Species or class not valid");
-            }
+        //    if(species == null || @class == null)
+        //    {
+        //        throw new InvalidOperationException("Species or class not valid");
+        //    }
 
-            var character = CharacterMapper.ToEntity(createCharacterDto, species, @class);
-            await _characterRepository.AddAsync(character);
+        //    var character = CharacterMapper.ToEntity(createCharacterDto, species, @class);
+        //    await _characterRepository.AddAsync(character);
 
-            return CharacterMapper.ToDto(character);
-        }
+        //    return CharacterMapper.ToDto(character);
+        //}
 
-        public async Task<bool> DeleteAsync(Guid id)
-        {
-            var character = await _characterRepository.GetByIdAsync(id);
-            if(character is null)
-            {
-                return false;
-            }
+        //public async Task<bool> DeleteAsync(Guid id)
+        //{
+        //    var character = await _characterRepository.GetByIdAsync(id);
+        //    if(character is null)
+        //    {
+        //        return false;
+        //    }
 
-            await _characterRepository.DeleteAsync(character);
-            return true;
-        }
+        //    await _characterRepository.DeleteAsync(character);
+        //    return true;
+        //}
 
-        public async Task<CharacterDto?> GetByIdAsync(Guid id)
-        {
-            var character = await _characterRepository.GetByIdAsync(id);
-            if (character is null)
-            {
-                throw new NotFoundException("Character not found");
-            }
+        //public async Task<CharacterDto?> GetByIdAsync(Guid id)
+        //{
+        //    var character = await _characterRepository.GetByIdAsync(id);
+        //    if (character is null)
+        //    {
+        //        throw new NotFoundException("Character not found");
+        //    }
 
-            return CharacterMapper.ToDto(character);
-        }
+        //    return CharacterMapper.ToDto(character);
+        //}
 
-        public async Task<CharacterDto?> UpdateAsync(Guid id, CreateCharacterDto dto)
-        {
-            var species = await _speciesRepository.GetByIdAsync(dto.SpeciesId);
-            var @class = await _classRepository.GetByIdAsync(dto.ClassId);
+        //public async Task<CharacterDto?> UpdateAsync(Guid id, CreateCharacterDto dto)
+        //{
+        //    var species = await _speciesRepository.GetByIdAsync(dto.SpeciesId);
+        //    var @class = await _classRepository.GetByIdAsync(dto.ClassId);
 
-            if (species is null || @class is null)
-            {
-                throw new InvalidOperationException("Species or class not valid");
-            }
+        //    if (species is null || @class is null)
+        //    {
+        //        throw new InvalidOperationException("Species or class not valid");
+        //    }
 
-            var character = await _characterRepository.GetByIdAsync(id);
+        //    var character = await _characterRepository.GetByIdAsync(id);
 
-            if(character is null)
-            {
-                throw new NotFoundException("Character not found");
-            }
+        //    if(character is null)
+        //    {
+        //        throw new NotFoundException("Character not found");
+        //    }
 
-            character.Update(dto.Name, species, @class, dto.Level, dto.HitPoints, dto.ArmorClass);
+        //    character.Update(dto.Name, species, @class, dto.Level, dto.HitPoints, dto.ArmorClass);
 
-            await _characterRepository.UpdateAsync(character);
-            return CharacterMapper.ToDto(character);
-        }
+        //    await _characterRepository.UpdateAsync(character);
+        //    return CharacterMapper.ToDto(character);
+        //}
     }
 }
