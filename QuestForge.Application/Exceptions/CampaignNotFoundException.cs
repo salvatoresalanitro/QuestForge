2f3c33393524f9c83fb76414774d58092c5f0c5f
Salvo <salvatoresalanitro94@gmail.com>
2025-07-11 12:19:21 +0200

    create new application exception and campaign not found exception

---
+++ QuestForge.Application/Exceptions/CampaignNotFoundException.cs
@@ -0,0 +1,11 @@
+﻿namespace QuestForge.Application.Exceptions
+{
+    public class CampaignNotFoundException : ApplicationException
+    {
+        public CampaignNotFoundException() { }
+
+        public CampaignNotFoundException(string message) : base(message) { }
+
+        public CampaignNotFoundException(string message, Exception innerException) : base(message, innerException) { }
+    }
+}
