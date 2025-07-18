d6cd59bc403feda82592eace8d132499c765d185
Salvatore Salanitro <salvatoresalanitro94@gmail.com>
2025-07-18 12:41:29 +0200

    add use case of update campaign, command and handler

---
+++ QuestForge.Application/UsesCases/Commands/Campaigns/UpdateCampaign/UpdateCampaignCommand.cs
@@ -0,0 +1,19 @@
+﻿using MediatR;
+using QuestForge.DTOs.DTOsCampaign;
+
+namespace QuestForge.Application.UsesCases.Commands.Campaigns.UpdateCampaign
+{
+    public sealed record UpdateCampaignCommand : IRequest<CampaignDto>
+    {
+        public Guid Id { get; }
+        public string Name { get; } = string.Empty;
+        public string Description { get; } = string.Empty;
+
+        public UpdateCampaignCommand(Guid id, string name, string description)
+        {
+            Id = id;
+            Name = name;
+            Description = description;
+        }
+    }
+}
---
+++ QuestForge.Application/UsesCases/Commands/Campaigns/UpdateCampaign/UpdateCampaignCommandHandler.cs
@@ -0,0 +1,34 @@
+﻿using MediatR;
+using QuestForge.Application.Exceptions;
+using QuestForge.Application.Mapping;
+using QuestForge.Domain.Campaigns;
+using QuestForge.DTOs.DTOsCampaign;
+
+namespace QuestForge.Application.UsesCases.Commands.Campaigns.UpdateCampaign
+{
+    public sealed record UpdateCampaignCommandHandler : IRequestHandler<UpdateCampaignCommand, CampaignDto>
+    {
+        private readonly ICampaignRepository _repository;
+
+        public UpdateCampaignCommandHandler(ICampaignRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<CampaignDto> Handle(UpdateCampaignCommand request, CancellationToken cancellationToken)
+        {
+            var campaign = await _repository.GetByIdAsync(request.Id, cancellationToken);
+
+            if(campaign is null)
+            {
+                throw new CampaignNotFoundException("Campaign not found.");
+            }
+
+            campaign.Update(request.Name, request.Description);
+
+            await _repository.UpdateAsync(campaign, cancellationToken);
+
+            return CampaignMapper.ToDto(campaign);
+        }
+    }
+}
