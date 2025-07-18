6a6713d022330ac81e1ab74e47be1e1260587de1
Salvatore Salanitro <salvatoresalanitro94@gmail.com>
2025-07-18 12:40:39 +0200

    update sign interface of update method

--- QuestForge.Domain/Campaigns/ICampaignRepository.cs
+++ QuestForge.Domain/Campaigns/ICampaignRepository.cs
@@ -5,7 +5,7 @@
         Task<Campaign?> GetByIdAsync(Guid campaignId, CancellationToken cancellationToken);
         Task<IEnumerable<Campaign>> GetAllAsync(CancellationToken cancellationToken);
         Task CreateAsync(Campaign campaign, CancellationToken cancellationToken);
-        Task UpdateAsync(Campaign campaign);
+        Task UpdateAsync(Campaign campaign, CancellationToken cancellationToken);
         Task DeleteAsync(Campaign campaign);
     }
 }
