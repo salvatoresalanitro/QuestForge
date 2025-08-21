4409e9bfb6e07b85fc821b15faaea2e1178a2a76
Salvo <salvatoresalanitro94@gmail.com>
2025-08-21 14:44:24 +0200

    add getall character query and handler

---
+++ QuestForge.Application/UsesCases/Queries/Characters/GetAllCharacters/GetAllCharactersQueryHandler.cs
@@ -0,0 +1,26 @@
+﻿using MediatR;
+using QuestForge.Application.Mapping;
+using QuestForge.Domain.Characters;
+using QuestForge.DTOs.DTOsCharacter;
+
+namespace QuestForge.Application.UsesCases.Queries.Characters.GetAllCharacters
+{
+    internal class GetAllCharactersQueryHandler : IRequestHandler<GetAllCharactersQuery, IEnumerable<CharacterDto>>
+    {
+        private readonly ICharacterRepository _characterRepository;
+
+        public GetAllCharactersQueryHandler(ICharacterRepository characterRepository)
+        {
+            _characterRepository = characterRepository;
+        }
+
+        public async Task<IEnumerable<CharacterDto>> Handle(GetAllCharactersQuery request, CancellationToken cancellationToken)
+        {
+            var characters = await _characterRepository.GetAllAsync(cancellationToken);
+
+            return characters is null
+                ? throw new Exception("No characters found.")
+                : characters.Select(c => CharacterMapper.ToDto(c));
+        }
+    }
+}
