20cf0652c1279d29c5139d81cba8a5d7d6d3179e
Salvo <salvatoresalanitro94@gmail.com>
2025-08-21 10:06:26 +0200

    create new exceptions for class and species not found

---
+++ QuestForge.Application/Exceptions/ClassNotFoundException.cs
@@ -0,0 +1,11 @@
+﻿namespace QuestForge.Application.Exceptions
+{
+    internal class ClassNotFoundException : AppException
+    {
+        internal ClassNotFoundException() { }
+
+        internal ClassNotFoundException(string message) : base(message) { }
+
+        internal ClassNotFoundException(string message, Exception innerException) : base(message, innerException) { }
+    }
+}
---
+++ QuestForge.Application/Exceptions/SpeciesNotFoundException.cs
@@ -0,0 +1,11 @@
+﻿namespace QuestForge.Application.Exceptions
+{
+    internal class SpeciesNotFoundException : AppException
+    {
+        internal SpeciesNotFoundException() { }
+
+        internal SpeciesNotFoundException(string message) : base(message) { }
+
+        internal SpeciesNotFoundException(string message, Exception innerException) : base(message, innerException) { }
+    }
+}
