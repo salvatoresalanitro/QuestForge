38a34a1c0d9b4450beef0ea5e13ea8041e3ad683
Salvo <salvatoresalanitro94@gmail.com>
2025-08-21 10:04:54 +0200

    rename applicationException to appException due the already existent name

---
+++ QuestForge.Application/Exceptions/AppException.cs
@@ -0,0 +1,11 @@
+﻿namespace QuestForge.Application.Exceptions
+{
+    public class AppException : Exception
+    {
+        public AppException() { }
+
+        public AppException(string message) : base(message) { }
+
+        public AppException(string message, Exception innerException) : base(message, innerException) { }
+    }
+}
