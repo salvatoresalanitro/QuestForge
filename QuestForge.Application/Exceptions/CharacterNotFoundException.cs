b50cc94512bc7ec22403cd6fdcb7c85e7f58790e
Salvo <salvatoresalanitro94@gmail.com>
2025-08-21 14:51:00 +0200

    add new custom exception no character found

---
+++ QuestForge.Application/Exceptions/CharacterNotFoundException.cs
@@ -0,0 +1,11 @@
+﻿namespace QuestForge.Application.Exceptions
+{
+    internal class CharacterNotFoundException : AppException
+    {
+        internal CharacterNotFoundException() { }
+
+        internal CharacterNotFoundException(string message) : base(message) { }
+
+        internal CharacterNotFoundException(string message, Exception innerException) : base(message, innerException) { }
+    }
+}
