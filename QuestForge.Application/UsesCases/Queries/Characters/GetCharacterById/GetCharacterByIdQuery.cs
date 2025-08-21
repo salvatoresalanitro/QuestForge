7d4ee6b7658fa74cad1b3a483f7c8a0f5128c229
Salvo <salvatoresalanitro94@gmail.com>
2025-08-21 15:13:07 +0200

    add new query get by id character

---
+++ QuestForge.Application/UsesCases/Queries/Characters/GetCharacterById/GetCharacterByIdQuery.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+using QuestForge.DTOs.DTOsCharacter;
+
+namespace QuestForge.Application.UsesCases.Queries.Characters.GetCharacterById
+{
+    public sealed record GetCharacterByIdQuery : IRequest<CharacterDto>
+    {
+        public Guid Id { get; }
+
+        public GetCharacterByIdQuery(Guid id)
+        {
+            Id = id;
+        }
+    }
+}
