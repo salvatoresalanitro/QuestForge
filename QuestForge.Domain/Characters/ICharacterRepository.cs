e45a6ab6d125c4e3746353a2d69f0beac7f16707
Salvo <salvatoresalanitro94@gmail.com>
2025-08-22 09:33:24 +0200

    update interface icharacter repositories to add cancellation token

--- QuestForge.Domain/Characters/ICharacterRepository.cs
+++ QuestForge.Domain/Characters/ICharacterRepository.cs
@@ -5,7 +5,7 @@
         Task<Character?> GetByIdAsync(Guid characterId, CancellationToken cancellationToken);
         Task<IEnumerable<Character>> GetAllAsync(CancellationToken cancellationToken);
         Task CreateAsync(Character character, CancellationToken cancellationToken);
-        Task UpdateAsync(Character character);
-        Task DeleteAsync(Character character);
+        Task UpdateAsync(Character character, CancellationToken cancellationToken);
+        Task DeleteAsync(Character character, CancellationToken cancellationToken);
     }
 }
