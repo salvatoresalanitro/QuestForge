8b7183f11d506c081283dbb670adef0d09795364
Salvo <salvatoresalanitro94@gmail.com>
2025-08-22 09:53:40 +0200

    add delete character command and handler

---
+++ QuestForge.Application/UsesCases/Commands/Characters/DeleteCharacter/DeleteCharacterCommand.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+
+namespace QuestForge.Application.UsesCases.Commands.Characters.DeleteCharacter
+{
+    public sealed record DeleteCharacterCommand : IRequest<bool>
+    {
+        public Guid Id { get; }
+
+        public DeleteCharacterCommand(Guid id)
+        {
+            Id = id;
+        }
+    }
+}
---
+++ QuestForge.Application/UsesCases/Commands/Characters/DeleteCharacter/DeleteCharacterCommandHandler.cs
@@ -0,0 +1,28 @@
+﻿using MediatR;
+using QuestForge.Domain.Characters;
+
+namespace QuestForge.Application.UsesCases.Commands.Characters.DeleteCharacter
+{
+    public sealed record DeleteCharacterCommandHandler : IRequestHandler<DeleteCharacterCommand, bool>
+    {
+        private readonly ICharacterRepository _characterRepository;
+
+        public DeleteCharacterCommandHandler(ICharacterRepository characterRepository)
+        {
+            _characterRepository = characterRepository;
+        }
+
+        public async Task<bool> Handle(DeleteCharacterCommand request, CancellationToken cancellationToken)
+        {
+            var character = await _characterRepository.GetByIdAsync(request.Id, cancellationToken);
+
+            if(character is null)
+            {
+                return false;
+            }
+
+            await _characterRepository.DeleteAsync(character, cancellationToken);
+            return true;
+        }
+    }
+}
