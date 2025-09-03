cd22a1c50bab7c00d7a9edb12bc569a11c8865ca
Salvo <salvatoresalanitro94@gmail.com>
2025-09-03 17:29:21 +0200

    add check during update of armor class

--- QuestForge.Domain/Characters/CharacterVO/ArmorClass.cs
+++ QuestForge.Domain/Characters/CharacterVO/ArmorClass.cs
@@ -23,6 +23,11 @@ namespace QuestForge.Domain.Characters.CharacterVO
 
         public void Update(int value)
         {
+            if (value < 0)
+            {
+                throw new CharacterUpdateException("Armor class cannot be negative.");
+            }
+
             Value = value;
         }
 
