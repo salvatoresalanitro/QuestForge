16a44687fabd930a91288ca7d6d6c2e3d8563101
Salvo <salvatoresalanitro94@gmail.com>
2025-09-17 10:44:36 +0200

    add new update campaign name exception

---
+++ QuestForge.Domain/Common/Exceptions/CampaignUpdateException.cs
@@ -0,0 +1,11 @@
+﻿namespace QuestForge.Domain.Common.Exceptions
+{
+    public class CampaignUpdateException : DomainException
+    {
+        public CampaignUpdateException() { }
+
+        public CampaignUpdateException(string message) : base(message) { }
+
+        public CampaignUpdateException(string message, Exception innerException) : base(message, innerException) { }
+    }
+}
